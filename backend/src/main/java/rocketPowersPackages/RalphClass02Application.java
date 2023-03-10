3067a8b0d4c190a0d6ac55907476c6137290823a
rocketpowers <rocketpowerswillian@gmail.com>
2023-03-10 19:33:36 -0300

    productModel create - mysql on work

---
+++ backend/src/main/java/rocketPowersPackages/RalphClass02Application.java
@@ -0,0 +1,13 @@
+package rocketPowersPackages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RalphClass02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RalphClass02Application.class, args);
+	}
+
+}
